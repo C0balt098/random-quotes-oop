5228653a8cbbcbb00bce6397d9a8892c0643ae68
YarovoiMykola <yrovau3@gmail.com>
2025-08-01 21:21:42 +0300

    Format quote text and author in the quote class

--- src/classes/RandomQuotesApp.js
+++ src/classes/RandomQuotesApp.js
@@ -7,16 +7,15 @@ class RandomQuotesApp {
     this.currentQuote = null;
     this.init();
   }
+  displayCurrentQuote() {
+    const {text, author} = this.currentQuote;
+    this.quoteTextElement.textContent = this.currentQuote.formatText();
+    this.quoteAuthorElement.textContent = this.currentQuote.formatAuthor();
+  }
   getRandomQuote() {
     const randomQuote = RandomQuote.getRandomQuote();
     this.currentQuote = randomQuote;
     this.displayCurrentQuote();
-  }
-
-  displayCurrentQuote() {
-    const {text, author} = this.currentQuote;
-    this.quoteTextElement.textContent = `"${text}"`;
-    this.quoteAuthorElement.textContent = author;
   }
 
   init() {
