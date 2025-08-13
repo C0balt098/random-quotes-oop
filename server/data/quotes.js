2bfb2a622d35a0c5d5c35f7e08b8dfe460b937aa
YarovoiMykola <yrovau3@gmail.com>
2025-08-13 21:32:52 +0300

    Removed old server and created own, server response to request for one random quote and server response about full list of quotes

---
+++ server/data/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    id: "a1b2c3d4",
+    text: "Do what you can, with what you have, where you are.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: "b2c3d4e5",
+    text: "It always seems impossible until it’s done.",
+    author: "Nelson Mandela",
+  },
+  {
+    id: "c3d4e5f6",
+    text: "Happiness depends upon ourselves.",
+    author: "Aristotle",
+  },
+  {
+    id: "d4e5f6g7",
+    text: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein",
+  },
+  {
+    id: "e5f6g7h8",
+    text: "Don’t count the days, make the days count.",
+    author: "Muhammad Ali",
+  },
+  {
+    id: "f6g7h8i9",
+    text: "Whether you think you can or you think you can’t, you’re right.",
+    author: "Henry Ford",
+  },
+  {
+    id: "g7h8i9j0",
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    id: "h8i9j0k1",
+    text: "Act as if what you do makes a difference. It does.",
+    author: "William James",
+  },
+  {
+    id: "i9j0k1l2",
+    text: "Opportunities don't happen, you create them.",
+    author: "Chris Grosser",
+  },
+  {
+    id: "j0k1l2m3",
+    text: "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
+  },
+];
+
+module.exports = quotes;
